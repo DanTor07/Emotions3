7c61a5e3dfed9a97015cbe410bc9851e76f7d5b0
DanTor07 <125132273+DanTor07@users.noreply.github.com>
2023-11-26 16:15:14 -0500

    "Modificación estructura y orden"

---
+++ Data/Connection.cs
@@ -0,0 +1,76 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using static Google.Rpc.Context.AttributeContext.Types;
+
+namespace Borrador3Proyecto
+{
+    public class Connection
+    {
+        public FirestoreDb FirestoreDb { get; set; }
+        public string Autor { get; set; }
+        public string SelectedEmotion { get; set; } // Nueva propiedad para almacenar la emoción seleccionada por el usuario
+
+        public Connection(string selectedEmotion)
+        {
+            SelectedEmotion = selectedEmotion;
+            Autor = "Rage Against the Machine"; // Cambia esto según tu lógica para obtener el autor
+            var filePath = @"Data\musicemotions-cf347-firebase-adminsdk-ld0qe-2a4f43257e.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filePath);
+            FirestoreDb = FirestoreDb.Create("musicemotions-cf347");
+        }
+
+        public void InsertEmotionAndAuthor()
+        {
+            Emotion emotion = new Emotion(string.Empty, SelectedEmotion/*, Autor*/);
+
+            var fbModel = MapEntityToFirestoreModel(emotion);
+            var colRef = FirestoreDb.Collection("Emotion");
+            var doc = colRef.AddAsync(fbModel).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        public void InsertEmotionAndSongInfo(MusicInfo songInfo)
+        {
+            Emotion emotion = new Emotion(string.Empty, SelectedEmotion/*, Autor*/);
+
+            var fbModel = MapEntityToFirestoreModel(emotion);
+            var colRef = FirestoreDb.Collection("Emotion");
+            var doc = colRef.AddAsync(fbModel).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            // Obtener el ID del documento recién creado
+            string emotionId = doc.Id;
+
+            // Actualizar el documento con la información de la canción
+            var emotionDocument = colRef.Document(emotionId);
+            var emotionData = emotionDocument.GetSnapshotAsync().ConfigureAwait(false).GetAwaiter().GetResult().ConvertTo<Data.FirestoreModels.EmotionFirestoreModel>();
+
+            if (emotionData.MusicList == null)
+            {
+                emotionData.MusicList = new List<Data.FirestoreModels.MusicInfoFirestoreModel>();
+            }
+
+            emotionData.MusicList.Add(new Data.FirestoreModels.MusicInfoFirestoreModel
+            {
+                Álbum = songInfo.Álbum,
+                Autor = songInfo.Autor,
+                Género = songInfo.Género,
+                URL_de_la_Imagen_del_Álbum = songInfo.URL_de_la_Imagen_del_Álbum,
+                URL_de_la_Imagen_del_Artista = songInfo.URL_de_la_Imagen_del_Artista,
+                URL_del_Video = songInfo.URL_del_Video
+            });
+
+            // Actualizar los datos de la emoción en Firestore
+            emotionDocument.SetAsync(emotionData, SetOptions.MergeAll).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        private Data.FirestoreModels.EmotionFirestoreModel MapEntityToFirestoreModel(Emotion emotion)
+        {
+            return new Data.FirestoreModels.EmotionFirestoreModel
+            {
+                Id = emotion.Id,
+                Emocion = emotion.Emocion,
+                //Autor = emotion.Autor
+            };
+        }
+    }
+}
---
+++ Emotion.cs
@@ -0,0 +1,33 @@
+﻿using Google.Cloud.Firestore;
+using System.Security.Cryptography;
+using static Google.Rpc.Context.AttributeContext.Types;
+using System.Collections.Generic;  // Agregar este using para List
+
+namespace Borrador3Proyecto
+{
+    [FirestoreData]
+    public class Emotion
+    {
+        [FirestoreDocumentId]
+        private string _id { get; set; }
+
+        [FirestoreProperty]
+        private string _Emocion { get; set; }
+
+        [FirestoreProperty]
+        public List<MusicInfo> MusicList { get; set; } = new List<MusicInfo>();  // Inicializar la lista
+
+        public string Id { get { return _id; } set { _id = value; } }
+
+        public string Emocion { get { return _Emocion; } set { _Emocion = value; } }
+
+        public string Autor { get; set; }
+
+        public Emotion(string id, string emocion/*, string autor*/)
+        {
+            Id = id;
+            Emocion = emocion;
+            //Autor = autor;
+        }
+    }
+}
